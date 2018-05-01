04507321f79559354c6debaab710b9f14b2e8774
mahesh <mahesh2150@gmail.com>
2018-05-01 09:38:19 +0530

    added meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private meta: Meta) {
+    console.log(this.meta)
+    this.meta.addTag({ name: 'description', content: 'How to use Angular 4 meta service' });
+    this.meta.addTag({ name: 'author', content: 'talkingdotnet' });
+    this.meta.addTag({ name: 'keywords', content: 'Angular, Meta Service' });
+  }
+  ngOnInit() {
+    console.log(this.meta)
+  }
+
 }
